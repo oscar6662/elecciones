c7dc425bf6a475b971724e52de3adafd44ad333c
oscar6662 <oscar.berenguer6@gmail.com>
2021-05-19 16:01:46 +0000

    heroku

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-
-ReactDOM.render(
+render((
   <BrowserRouter>
-      <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+      <App/>
+  </BrowserRouter>
+), document.getElementById('root'));
